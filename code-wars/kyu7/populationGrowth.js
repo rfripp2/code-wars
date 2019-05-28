b7dc373aceab6aa70c5b06e45bbfc05bc26d9eff
rfripp2 <juangonzalez465@gmail.com>
2019-05-28 00:11:24 -0300

    removing duplicated directory

--- code-wars/kyu7/populationGrowth.js
+++
@@ -1,7 +0,0 @@
-function nbYear(p0, percent, aug, p) {
-  let yearCounter = 0;
-  for (let i = yearCounter; p0 < p; yearCounter++) {
-    p0 += p0 * (percent / 100) + aug;
-  }
-  return yearCounter;
-}
